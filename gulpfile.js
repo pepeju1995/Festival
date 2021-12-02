c0c4337cd395b7d16a8b33033608fe92e4612d4c
pepeju1995 <pepeju95@gmail.com>
2021-12-02 16:10:04 +0100

    config gulp an scss variables

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,23 @@
-const { src, dest } = require('gulp');
-const sass = require('gulp-sass');
+const { src, dest, watch } = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const plumber = require('gulp-plumber');
 
 
 function css( done ) {
     
-    src('src/scss/app.scss')// Identificar el archivo .scss a compilar
+    src('src/scss/**/*.scss')// Identificar el archivo .scss a compilar
+        .pipe( plumber() )
         .pipe( sass() ) // Compilarlo
         .pipe( dest('build/css') ) // Almacenar   
     done();
 }
 
+function dev( done) {
+    
+    watch('src/scss/**/*.scss', css);
+
+    done();
+}
+
 exports.css = css;
+exports.dev = dev;
